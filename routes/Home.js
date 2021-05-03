6af9dbe37e5bf3fc261ca6b03ed25f73f8d4405a
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-05-03 15:04:19 -0700

    work done on users paths

--- routes/Home.js
+++ routes/Home.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { asyncHandler } = require('./utils')
+const { User } = require('../db/models')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('Home', { title: 'a/A Express Skeleton Home', thing: res.locals.authenticated });
-});
+router.get('/', asyncHandler(async (req, res, next) => {
+  let user;
+  if (res.locals.authenticated) {
+    user = res.locals.user;
+  }
+  res.render('Home', { title: 'a/A Express Skeleton Home', user });
+}));
 
 module.exports = router;
--- routes/Users.js
+++ routes/Users.js
@@ -1,19 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { asyncHandler } = require('./utils')
+const { User, Follower, Like, Story, Comment } = require('../db/models')
 
 
-router.get('/', function(req, res, next) {
+router.get('/:id', asyncHandler(async (req, res, next) => {
+    let currentUser = false;
+    if (req.params.id == res.locals.user) {
+        currentUser = true
+    };
+    const user = res.locals.user
+    res.render('User', { user, currentUser }) //if currentUser is true, giev extra privlages
+}));
 
-});
+
+router.post('/:id/follow', asyncHandler(async (req, res, next) => {
+    await Follower.create({
+        follower_userid: res.locals.user,
+        following_userid: req.params.id,
+    })
+}))
 
 
-router.post('/:id/follow', async(req, res, next) => {
-
-})
-
-
-router.delete('/:id/follow', async(req, res, next) => {
-
+router.delete('/:id/follow', async (req, res, next) => {
+    await Follower.delete({
+        where: {
+            follower_userid: res.locals.user,
+            following_userid: req.params.id,
+        }
+    })
 })
 
 
