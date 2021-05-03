09c1fdd1dcfaf711cb9daad80b868ed5046c51c2
Nicholas Brancifort <brancifortnick@yahoo.com>
2021-05-03 14:03:40 -0400

    UserModelEditing

---
+++ db/migrations/20210503173755-create-like.js
@@ -0,0 +1,37 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Likes", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: { model: "Users" },
+      },
+      stories_id: {
+        type: Sequelize.INTEGER,
+        references: { model: "Stories" },
+      },
+      comment_id: {
+        type: Sequelize.INTEGER,
+        references: { model: "Comments" },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Likes");
+  },
+};
