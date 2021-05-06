f390667d3a19797a53abe45680210ad4e1021030
gjetost1 <rosen659@umn.edu>
2021-05-06 15:03:55 -0500

    story imgs

--- db/seeders/20210504172252-stories.js
+++ db/seeders/20210504172252-stories.js
@@ -21,7 +21,7 @@ module.exports = {
       author_id: 1,
       createdAt: new Date(),
       updatedAt: new Date(),
-      storyImage: '/exampleImgs/adventure.jpg'
+      storyImage: '/storyImgs/nightmare.png'
     },{
       title: 'Charming Baby Leaf',
       story: `Baby Leaf had always loved damp Bangkok with its happy, handsome hills. It was a place where she felt ambivalent.
@@ -42,7 +42,7 @@ module.exports = {
       author_id: 2,
       createdAt: new Date(),
       updatedAt: new Date(),
-      storyImage: '/exampleImgs/babyLeaf.jpg'
+      storyImage: '/storyImgs/zorldo.png'
     },{
       title: 'Hilarious Mozarella',
       story: `Mozarella had always loved grey West Boggins with its jolly, joyous jungle. It was a place where she felt happy.
@@ -63,7 +63,7 @@ module.exports = {
       author_id: 3,
       createdAt: new Date(),
       updatedAt: new Date(),
-      storyImage: '/exampleImgs/funnyCheese.jpg'
+      storyImage: '/storyImgs/prince.png'
     },{
       title: 'Two Adorable Uncles',
       story: `Luke Lakeman had always loved idyllic Dallas with its energetic, enthusiastic estuaries. It was a place where he felt surprised.
@@ -84,7 +84,7 @@ module.exports = {
       author_id: 1,
       createdAt: new Date(),
       updatedAt: new Date(),
-      storyImage: '/exampleImgs/family.jpg'
+      storyImage: '/storyImgs/nightmare.png'
     },{
       title: 'Two Virtuous Uncles',
       story: `Josh Trescothik looked at the weathered map in his hands and felt sleepy.
@@ -108,7 +108,7 @@ module.exports = {
       author_id: 1,
       createdAt: new Date(),
       updatedAt: new Date(),
-      storyImage: '/exampleImgs/family.jpg'
+      storyImage: '/storyImgs/punch.png'
     }, {
       title: 'Whatever Happened To Roguelikes?',
       story: `Back when I was a kid, all we needed to have fun was a procedurally generated map and whole mess of RNG loot. Ahhh, the good old days.
@@ -131,7 +131,7 @@ module.exports = {
       author_id: 1,
       createdAt: new Date(),
       updatedAt: new Date(),
-      storyImage: '/exampleImgs/family.jpg'
+      storyImage: '/storyImgs/fight.png'
     }, {
       title: 'Designing Player Punishment',
       story: `My studio is designing a video game where you watch the contestants slowly gaining weight, endure insults and curses, and try to win the game back!
@@ -145,7 +145,7 @@ module.exports = {
       author_id: 2,
       createdAt: new Date(),
       updatedAt: new Date(),
-      storyImage: '/exampleImgs/family.jpg'
+      storyImage: '/storyImgs/nightmare.png'
     }, {
       title: 'Modern Horror Games',
       story: `I would like to program a new video game that could be made from this concept.
@@ -159,7 +159,7 @@ module.exports = {
       author_id: 3,
       createdAt: new Date(),
       updatedAt: new Date(),
-      storyImage: '/exampleImgs/family.jpg'
+      storyImage: '/storyImgs/dark.png'
     }, {
       title: 'We Need More Wizards',
       story: `I have an idea for a video game where the player watches the wizard interacting with the world, he talks to the character dialog, does battle doing some exercise occasionally, eats some food, sleeps, etc. The idea is that the video player would be able to hear the character do various tasks, but not just fight, err, do some chores, answer a call, walk around, do some shopping, etc. The watcher would be able to see character's movement as well as the character's face. And the display would be retina-forming so that the character looks like a wizened teenager.
@@ -177,7 +177,7 @@ module.exports = {
       author_id: 4,
       createdAt: new Date(),
       updatedAt: new Date(),
-      storyImage: '/exampleImgs/family.jpg'
+      storyImage: '/storyImgs/rpg.png'
     }, {
       title: 'Mastering Fighting Games',
       story: `Fighting games are about more than just mastering move combinations and mashing buttons.
@@ -195,94 +195,7 @@ module.exports = {
       author_id: 4,
       createdAt: new Date(),
       updatedAt: new Date(),
-      storyImage: '/exampleImgs/angryUncles.jpg'
-    }, {
-      title: 'Whatever Happened To Roguelikes?',
-      story: `Back when I was a kid, all we needed to have fun was a procedurally generated map and whole mess of RNG loot. Ahhh, the good old days.
-      Truly, I miss those days. Back when a young person could spend days losing themself in Nethack without a thought or care in the world for the mundanity of modern life.
-      Nowadays it's all fast-paced battle royale games, with gamers hopped up on vape smoke and energy drinks, ripping around on their Razers and Rosewills.
-      I heard a statistic the other day, in fact. Cherry Reds are all the rage, and over 60% of gamers now use a specialized gaming mouse and keyboard. What happened to the shallow, insanely repetitive gameplay?
-      What happened to the blocky, difficult to interpret graphics? To the uneven and asymmetrical AI enemies? To the simplistic map design?
-      What happened to the joy of gaming? It's all been ruined, if you ask me. And you are asking me! I'm right here.
-      Okay, grandpa, let's get you to bed.
-      Sorry about that. Pawpaw got onto my account again. See, my grandfather tends to wax rhapsodic about the old days, but he doesn't seem to understand that we still have roguelikes.
-      We still have all the stuff he liked, in fact. He just has a little trouble with information diffusion and filtering. The huge glut of games makes it hard for him to keep track of what's what.
-      And I can't blame him! There were a lot less options back in the day. People still like Miller Lite, after all. Sometimes they didn't have any other options!
-      I've been telling Pawpaw there's no reason to get upset, but he just tells me to turn up the Dig Dug. What can you do? I still love him.
-      Not a huge problem, though. He still comes around once in awhile, for a really good game. One day, I hope I'm like him. I hope I can still enjoy the novelty and innovation.
-      Even if it's hard to admit.
-      And it might be.
-      THE END`,
-      liked: 0,
-      views: 2000,
-      author_id: 1,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      storyImage: '/exampleImgs/angryUncles.jpg',
-    }, {
-      title: 'Designing Player Punishment',
-      story: `My studio is designing a video game where you watch the contestants slowly gaining weight, endure insults and curses, and try to win the game back!
-      I have this idea for a game idea that could be used as a template for something, just in case.
-      I am planning to make a survival game around this time of year, but I want to make something really special (like an RPG) around this time of year, so the idea must have something to with the special conditions that the humans in the movie went in for the biggest slump they've had to make a slump of sorts to stay alive. So the idea must be that it's a 2D game, you just shovel water on to people and freeze them in place for 30 seconds, with special properties.
-      Then once they make it, you are allowed to put them in your game and play them on their behalf.
-       I plan on 2d survival games, but might expand it to 3rd/4th depending on market share.
-      Thank you for reading! I don't know how hard this will probably be to make, but basically i ground my ideas flat on the floor, then waited it out, hoping something crazy wil`,
-      liked: 0,
-      views: 10,
-      author_id: 1,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      storyImage: '/exampleImgs/angryUncles.jpg'
-    }, {
-      title: 'Modern Horror Games',
-      story: `I would like to program a new video game that could be made from this concept.
-      Please share this with friends and other developers. I'm envisioning a game that I've been working on called "Under the Dome", and I would like to add some horror to it.
-      I'm not really into the type of games that you'd typically put yourself in, but I think that if there's a game that would fit within the Under the Dome concept, it'd be on the list of things that I need to build.
-      Also, there are a very very very small amount of clay planet types in this world out there, so there may or may not be dungeons or pits, or even entire continents. I think that for the most part, these are the types of things we're going to be putting into place for this game to make it feel natural.
-      Also, there are a very very very long amount of ways to go this game will go, and you can't really get good at it until you're 40, which is when you really start getting into character development.
-      This is all I can write for now, please let me know what you all think!`,
-      liked: 0,
-      views: 1004,
-      author_id: 3,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      storyImage: '/exampleImgs/angryUncles.jpg'
-    }, {
-      title: 'We Need More Wizards',
-      story: `I have an idea for a video game where the player watches the wizard interacting with the world, he talks to the character dialog, does battle doing some exercise occasionally, eats some food, sleeps, etc. The idea is that the video player would be able to hear the character do various tasks, but not just fight, err, do some chores, answer a call, walk around, do some shopping, etc. The watcher would be able to see character's movement as well as the character's face. And the display would be retina-forming so that the character looks like a wizened teenager.
-      The main point of the game is to see and hear the wizard say his prayers, as well as the conversation of the characters.
-      There would be a few apkkaized please-type features that I will get to.
-      Dialogue: I want the wizard to make helpful recordings letting me know what he is talking about, and when he is, I can show where he is going.
-      The apkkaized need to know the character's usual notes.
-      The idea is that you fight (and sometimes capture) NPCs with a joystick and the ability to switch between (sort of) classic TV shows and movies.
-      You have a party of three people, and after the victory is declared, all of the other players in the party are declared defeated and your game is called with a warning tape.
-      So for example, you would have three players in a table tennis match between three players, and after a little while they both count out and the match is called.
-      It would be video game like sports, but you can't penalize a play if your player doesn't have a showing in the sport.
-      In summary, this is a fun little game that players can experience and train for 3 - 5 hours.`,
-      liked: 0,
-      views: 976,
-      author_id: 3,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      storyImage: '/exampleImgs/angryUncles.jpg'
-    }, {
-      title: 'Mastering Fighting Games',
-      story: `Fighting games are about more than just mastering move combinations and mashing buttons.
-      I have an idea for a video game where you can duel but each player mans a character.
-      I need specific ideas on how to build a video game that's unique from other games that I have created.
-      What do you guys think? Thank you. I'm thinking something like this. I want to make a 3d game centered around wrestling.
-      It would be set in the '50s and '60s in a small Midwestern town. There would be a lot of characters but not a ton of action.
-      You might win by character instead of by character by character. Overall the game would be about doing what the show matches in: grappling.
-      In the '50s and '60s people in those years used to wrestling, but that wasn't a maintenence nor an option.
-      The focus would be on developing skills over time so you could advance through the sport without getting caught.
-      The catch would be that you're caught and you have to continue getting better at the sport.
-      In addition getting better at the sport would also unlock more special moves or powers. For example, if you win the match you can move on to the next round.`,
-      liked: 0,
-      views: 1276,
-      author_id: 4,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      storyImage: '/exampleImgs/angryUncles.jpg'
+      storyImage: '/storyImgs/wrestling.png'
     }
     ], {});
   },
